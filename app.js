3c87cca288642845f6ed82a08b23b6da206fd4fb
Wajahat-Ali-01 <hellogithub01@gmail.com>
2025-05-24 18:59:44 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const app = express();
+
+const http = require("http");
+const path = require("path");
+const socketio = require("socket.io");
+
+const server = http.createServer(app);
+const io = socketio(server);
+
+// Set view engine and static files
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+
+// Socket.io logic
+io.on("connection", function (socket) {
+  socket.on("send-location", function (data) {
+    io.emit("receive-location", { id: socket.id, ...data });
+  });
+
+  socket.on("disconnect", () => {
+    io.emit("user-diconnected", socket.id);
+  });
+});
+
+// Main route
+app.get("/", function (req, res) {
+  res.render("index");
+});
+
+// Start server
+server.listen(3000);
