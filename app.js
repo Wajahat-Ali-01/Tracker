c6089c9500da4dc4bcfd1293f41121f2dd96d3fc
Wajahat-Ali-01 <hellogithub01@gmail.com>
2025-05-27 10:04:59 +0500

    Update Project

--- app.js
+++ app.js
@@ -1,52 +1,32 @@
-// Import required modules
 const express = require("express");
+const app = express();
+
 const http = require("http");
 const path = require("path");
 const socketio = require("socket.io");
 
-const app = express(); // Initialize Express app
-const server = http.createServer(app); // Create HTTP server
-const io = socketio(server); // Attach Socket.IO to the server
+const server = http.createServer(app);
+const io = socketio(server);
 
-// Set view engine to EJS and define static files directory
+// Set view engine and static files
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "public")));
 
-// Store users and their locations
-const users = {};
-
-// Listen for new socket connections
+// Socket.io logic
 io.on("connection", function (socket) {
-  console.log("User connected:", socket.id);
-
-  // Send all existing user locations to the newly connected user
-  Object.keys(users).forEach((id) => {
-    socket.emit("receive-location", {
-      id: id,
-      ...users[id],
-    });
-  });
-
-  // Listen for a user's location and broadcast it to all users
   socket.on("send-location", function (data) {
-    users[socket.id] = data; // Save/update user's location
-    io.emit("receive-location", { id: socket.id, ...data }); // Broadcast to all
+    io.emit("receive-location", { id: socket.id, ...data });
   });
 
-  // Handle disconnection
   socket.on("disconnect", () => {
-    delete users[socket.id]; // Remove user from the list
-    io.emit("user-diconnected", socket.id); // Notify others
+    io.emit("user-diconnected", socket.id);
   });
 });
 
-// Define the main route
+// Main route
 app.get("/", function (req, res) {
-  res.render("index"); // Render the index.ejs file
+  res.render("index");
 });
 
-// Start the server
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => {
-  console.log("Server is running on port", PORT);
-});
+// Start server
+server.listen(3000);
